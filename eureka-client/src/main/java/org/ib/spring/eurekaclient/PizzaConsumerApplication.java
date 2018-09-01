72452d366c4294911d8f8b67bdfe2e57908e29bd
ionutb83@yahoo.com <wb9k7t2jrt>
2018-09-01 08:23:47 +0200

    Remove useless ServiceDiscovery services

--- eureka-client/src/main/java/org/ib/spring/eurekaclient/PizzaConsumerApplication.java
+++ eureka-client/src/main/java/org/ib/spring/eurekaclient/PizzaConsumerApplication.java
@@ -1,23 +1,11 @@
 package org.ib.spring.eurekaclient;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -37,42 +25,5 @@ public class PizzaConsumerApplication {
     @Bean
     public HomePageHelp getHomePageHelp(){
         return new HomePageHelp();
-    }
-}
-
-@Component
-class DiscoveryClientExample implements CommandLineRunner {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    private static final Log logger = LogFactory.getLog(DiscoveryClientExample.class);
-
-    @Override
-    public void run(String... strings) throws Exception {
-        logger.info("Providers list :");
-        discoveryClient.getInstances("pizzaprovider").forEach((ServiceInstance s) -> {
-            logger.info(ToStringBuilder.reflectionToString(s));
-        });
-    }
-}
-
-@Component
-class EurekaClientExample implements CommandLineRunner {
-
-    private EurekaClient eurekaClient;
-
-    private static final Log logger = LogFactory.getLog(DiscoveryClientExample.class);
-
-    public EurekaClientExample(EurekaClient eurekaClient) {
-        this.eurekaClient = eurekaClient;
-    }
-
-    @Override
-    public void run(String... strings) {
-        logger.info("Providers list :");
-        Stream.of(eurekaClient.getNextServerFromEureka("pizzaprovider", false)).forEach( (InstanceInfo i) -> {
-            logger.info(i.getHomePageUrl());
-        });
     }
 }
