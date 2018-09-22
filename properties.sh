d900fd69431d7abd1495209c157177579446df67
ionutb83@yahoo.com <wb9k7t2jrt>
2018-09-22 21:13:00 +0200

    Update tests

--- properties.sh
+++ properties.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-JMC_ARGS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=delay=1s,duration=99999s,disk=true,filename="
+JMC_ARGS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=stackdepth=1024 -XX:StartFlightRecording=delay=1s,duration=99999s,disk=true,filename="
 export JMC_ARGS
